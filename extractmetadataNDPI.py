43b8427e8fb064051899b78b270c18355d9bba80
simonpcastillo <spcastil@uc.cl>
2020-11-22 20:26:09 +0000

    Create extractmetadataNDPI.py

---
+++ extractmetadataNDPI.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+import glob
+from datetime import date
+import openslide
+
+inputfolder = "E:/GBM/gdrive/2020/H&E" #"E:/GBM/OneDrive_1_02-11-2020"
+outputfolder = "C:/Users/tinyhp/Dropbox (ICR)/GBM_simon/GBM_Quezada"  #"E:/GBM/OneDrive_1_02-11-2020"
+outputname = "metadata"
+
+#list files in inputfolder
+files = (glob.glob(inputfolder + "/*.ndpi"))
+
+colID = ["filename","magnification", "width(px)", "height(px)", "mpp-x", "mpp-y"]
+df = pd.DataFrame(columns= colID)
+
+#read each file and extract metadata
+for i in files:
+    wsi_path2 = i
+    t = openslide.open_slide(wsi_path2)
+    width = (t.properties['openslide.level[0].width'])
+    height = (t.properties['openslide.level[0].height'])
+    Magn = (t.properties['hamamatsu.SourceLens'])
+    mppY = (t.properties['openslide.mpp-y'])
+    mppX = (t.properties['openslide.mpp-x'])
+    filemetadata = [i, Magn, height, width, mppX, mppY]
+    df.loc[i] = filemetadata
+  
+#create csv with metadata
+df.to_csv((outputfolder + "/" + outputname + "_" + date.today().strftime("%Y%m%d") + ".csv"), index=False)
