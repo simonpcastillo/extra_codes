0d56400b64f10d8dccba67a660e1d54aa8d68535
simonpcastillo <spcastil@uc.cl>
2020-11-23 21:18:59 +0000

    update

--- diversity_clusters.R
+++ diversity_clusters.R
@@ -1,8 +1,20 @@
-require(vegan)
-data <- read.csv("POET0109B.csv") # rows are communities columns are cell types
-#remove rows under the cluster 0
-
-H <- diversity(data[,c("n.C", "n.L")])
-simp <- diversity(data[,c("n.C", "n.L")], "simpson")
-
-diversity_clusters <- data.frame(Shannon = H, Simpson = simp)
+diversity_clusters <- function(df, types = c("n.C", "n.L")){
+  
+  if (!require("pacman")) install.packages("pacman")
+  pacman::p_load(vegan)
+  
+  "%ni%"  <- Negate("%in%")
+  
+  for(i in length(types)){
+    if(types[i] %ni% colnames(df)){stop(paste0("type -> ", types[i] ," <- does not exist in the input dataframe"))}
+  }
+  
+  if(length(types) < 2 ){warning("if input dataframe have 1 sp, a global diversity is computed")}
+  
+  H <- diversity(data[,types])
+  simp <- diversity(data[,types], "simpson")
+  
+  diversity_clusters <- data.frame(Shannon = H, Simpson = simp)
+  
+  return(diversity_clusters)
+}
--- morisita_clusters.R
+++ morisita_clusters.R
@@ -1,3 +1,16 @@
-require(vegan)
-#remove data cluster 0 
-morisitaClusters <- dispindmorisita(data[,c("n.C", "n.L")], unique.rm = TRUE)
+morisita_clusters <- function(df, types = c("n.C", "n.L"), unique.rm = TRUE){
+  
+  if (!require("pacman")) install.packages("pacman")
+  pacman::p_load(vegan)
+  
+  "%ni%"  <- Negate("%in%")
+  
+  for(i in length(types)){
+    if(types[i] %ni% colnames(df)){stop(paste0("type -> ", types[i] ," <- does not exist in the input dataframe"))}
+  }
+  
+
+  morisita_clusters <- dispindmorisita(data[,types])
+  
+  return(morisita_clusters)
+}
