a4d3a986642147fbfe6ecc90e895a085a0b98dda
simonpcastillo <spcastil@uc.cl>
2020-11-25 13:57:01 +0000

    Update accessingPolyscope.R

--- accessingPolyscope.R
+++ accessingPolyscope.R
@@ -1,6 +1,6 @@
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(RCurl)
-
-url <- "ftp://ftppath/www_logs/testfolder/test.csv"
+img.id <- "Path000008_201604141207"
+url <- paste0("sftp://polyscope.icr.ac.uk/var/www/polyzoomer/",img.id, "/page/*/*/*/annotations.txt" )
 text_data <- getURL(url, userpwd = "username:password", connecttimeout = 60)
 df <- read.csv(text = text_data)
