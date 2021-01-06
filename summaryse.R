8515a8b12570a73307a7cffedf6225731747082d
simonpcastillo <spcastil@uc.cl>
2021-01-06 17:32:44 +0000

    Create summaryse.R

---
+++ summaryse.R
@@ -0,0 +1,10 @@
+lower_ci <- function(mean, se, n, conf_level = 0.95){
+  lower_ci <- mean - qt(1 - ((1 - conf_level) / 2), n - 1) * se
+}
+upper_ci <- function(mean, se, n, conf_level = 0.95){
+  upper_ci <- mean + qt(1 - ((1 - conf_level) / 2), n - 1) * se
+}
+
+
+
+
